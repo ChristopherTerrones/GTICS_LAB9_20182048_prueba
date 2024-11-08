1130a116ef597ba1501427727448b52d43129db4
ChristopherTerrones <106935547+ChristopherTerrones@users.noreply.github.com>
2024-11-08 03:55:24 -0500

    lista de categorias

---
+++ src/main/java/com/example/prueba_lab9/entity/Categoria.java
@@ -0,0 +1,13 @@
+package com.example.prueba_lab9.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Categoria {
+    private String idCategory;
+    private String strCategory;
+    private String strCategoryThumb;
+    private String strCategoryDescription;
+}
