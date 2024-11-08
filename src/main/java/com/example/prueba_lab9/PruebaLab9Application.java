ba9d839d5a90c77ab6925b2efbe21f2d41264529
ChristopherTerrones <106935547+ChristopherTerrones@users.noreply.github.com>
2024-11-08 01:08:13 -0500

    commit inicial

---
+++ src/main/java/com/example/prueba_lab9/PruebaLab9Application.java
@@ -0,0 +1,13 @@
+package com.example.prueba_lab9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaLab9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaLab9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prueba_lab9/PruebaLab9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_lab9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaLab9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
